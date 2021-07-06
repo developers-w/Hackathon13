e286601ae40a88e87c5bc92642792929612d6f89
avl <web.developers.bf@gmail.com>
2021-07-05 19:04:56 -0500

    hachaton end

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
 import {data} from './dataBase.js';
 
 const cards=document.querySelector('.cards');
-cards.innerHTML =''; // quita la maquetacion de card para hacerlo dinamico
+
+const mostrarFormulario=document.getElementById('addItem');
+
+const cargarDatos=document.getElementById('formulario');
+
+const containerItem=document.getElementById('containerItem');
+
 
 let dat=data;
 
-
+//cargar items
 document.addEventListener('DOMContentLoaded',loadPage);
 
 
-
-
-
+//quitar items
 cards.addEventListener('click',quitarItem);
+
+
+//agregar item
+mostrarFormulario.addEventListener('click',mostrar)
+
+cargarDatos.addEventListener('submit', agregarItem)
+
+
 
 
 function loadPage(){
@@ -53,13 +65,45 @@ function loadPage(){
 
 function quitarItem(e){
     if(e.target.classList.contains('delete')){
+        
          const posicion=e.target.parentElement.id;
-         const nuevoArray = data.filter(el=> el.Id != posicion);
+        
+         const nuevoArray = dat.filter(el=> el.Id != posicion);
+         dat=[...nuevoArray];
 
         loadPage()
     }
+}
+function mostrar(e){
+    e.preventDefault();   
+
+  
+    containerItem.style.display='block';
+ 
+   
+  
+   
+    
+}
+function agregarItem(e){
+    e.preventDefault();
+    const obj={
+        Id:(dat.length+1),
+        image:image.value,
+        Nombre:nombre.value,
+        Telefono:telefono.value,
+        Correo:correo.value,
+        Pais:pais.value,
+        Descripcion:descripcion.value
+
+    }
+    data.push(obj);
+    mostrarFormulario.addEventListener('click',mostrar)
+    loadPage();
+    containerItem.style.display='none';
 
 }
+
 
 
 
